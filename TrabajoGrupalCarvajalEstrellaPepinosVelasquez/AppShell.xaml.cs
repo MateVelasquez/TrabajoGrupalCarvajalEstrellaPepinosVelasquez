22443e48ec8b87b0a313cd2edac922b2ed32854c
laboratorio <laboratorio@UPOAULA10218>
2022-11-21 08:17:50 -0500

    Agregar archivos de proyecto.

---
+++ TrabajoGrupalCarvajalEstrellaPepinosVelasquez/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TrabajoGrupalCarvajalEstrellaPepinosVelasquez;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TrabajoGrupalCarvajalEstrellaPepinosVelasquez/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TrabajoGrupalCarvajalEstrellaPepinosVelasquez;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
